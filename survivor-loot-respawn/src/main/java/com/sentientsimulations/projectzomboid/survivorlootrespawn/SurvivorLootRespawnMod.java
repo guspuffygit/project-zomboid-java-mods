4f11dc32aedb2685aee6fcb4e8e812f3e73fb55c
guspuffygit <guspuffy@gmail.com>
2026-06-10 16:17:20 -0400

    Survivor loot respawn init

---
+++ survivor-loot-respawn/src/main/java/com/sentientsimulations/projectzomboid/survivorlootrespawn/SurvivorLootRespawnMod.java
@@ -0,0 +1,30 @@
+package com.sentientsimulations.projectzomboid.survivorlootrespawn;
+
+import static io.pzstorm.storm.logging.StormLogger.LOGGER;
+
+import io.pzstorm.storm.core.StormClassTransformer;
+import io.pzstorm.storm.event.core.StormEventDispatcher;
+import io.pzstorm.storm.mod.ZomboidMod;
+import io.pzstorm.storm.util.StormEnv;
+import java.util.Collections;
+import java.util.List;
+
+public class SurvivorLootRespawnMod implements ZomboidMod {
+
+    @Override
+    public void registerEventHandlers() {
+        if (!StormEnv.isStormServer()) {
+            return;
+        }
+        LOGGER.debug("Registering event handler for {}", SurvivorLootRespawnMod.class.getName());
+        StormEventDispatcher.registerEventHandler(this);
+    }
+
+    @Override
+    public List<StormClassTransformer> getClassTransformers() {
+        if (!StormEnv.isStormServer()) {
+            return Collections.emptyList();
+        }
+        return Collections.emptyList();
+    }
+}
